7f25f7b05e3c14f57a9d67a858345026327618b3
JuliBucket <juliansaucedo92@gmail.com>
2024-09-06 09:17:31 -0300

    se actualizó el archivo config

--- backend-jwt/db/database.js
+++ backend-jwt/db/database.js
@@ -1,12 +1,13 @@
 import mysql2 from "mysql2/promise";
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } from "../config/config.js"
 
 export const newConnection = async () => {
   try {
     const connection = await mysql2.createConnection({
-      host: "localhost",
-      user: "root",
-      database: "db_system",
-      password: "",
+      host: DB_HOST || "localhost",
+      user: DB_USER || "root",
+      database: DB_NAME || "db_system",
+      password: DB_PASSWORD || "",
     });
     await connection.connect();
     console.log("conectado a la base de datos correctamente");
