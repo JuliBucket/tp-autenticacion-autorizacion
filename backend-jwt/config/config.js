d9c66cb10a1825b650e8b63c5377912caf0dad39
JuliBucket <juliansaucedo92@gmail.com>
2024-09-06 12:37:27 -0300

    se arreglo el archivo config

--- backend-jwt/config/config.js
+++ backend-jwt/config/config.js
@@ -3,3 +3,4 @@ export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_PASSWORD = ""
 export const DB_NAME = "db_system"
 export const DB_USER = "root"
+export const SECRET_KEY = 'aopz9-3f1iw12-4ja94f-6l4jzg8'
